40d72a43da1fdeb598e3f8d53ada1fc17af5f264
ChacaS0 <bastide.seb.pro@gmail.com>
2018-05-09 07:31:37 +0400

    sync hero service

--- 01-tuto/angular_tour_of_heroes/lib/app_component.dart
+++ 01-tuto/angular_tour_of_heroes/lib/app_component.dart
@@ -1,7 +1,8 @@
 import 'package:angular/angular.dart';
-import 'package:angular_forms/angular_forms.dart';
 import 'src/hero.dart';
 import 'src/mock_heroes.dart';
+import 'src/hero_detail_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
 	selector: 'app-component',
@@ -9,14 +10,18 @@ import 'src/mock_heroes.dart';
 	// template: '''
 	// <h1>{{title}}</h1> <h2>{{hero}} details!</h2>
 	// ''',
-	directives: const [CORE_DIRECTIVES, formDirectives],
-  styleUrls: const ['app_component.css'],
+	directives: const [CORE_DIRECTIVES, HeroDetailComponent],
+	styleUrls: const ['app_component.css'],
+	providers: const [HeroService],
 )
 
 
 
-class AppComponent {
+class AppComponent implements OnInit {
 	final title = 'Tour of Heroes';
+	final HeroService _heroService;
+
+	AppComponent(this._heroService);
 
 	// Hero h1 = new Hero(1, "Wildstorm");
 
@@ -26,5 +31,11 @@ class AppComponent {
 
 	// onSelect(Hero) event click handler
 	void onSelect(Hero hero) => selectedHero = hero;
+
+	// getter for heroes
+	void getHeroes() => heroes = _heroService.getHeroes();
+
+	// OnInit implementation
+	void ngOnInit() => getHeroes();
 }
 
