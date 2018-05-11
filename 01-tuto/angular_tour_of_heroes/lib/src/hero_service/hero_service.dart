d63810aeed98a0a55a679bf7ae2d874790d597b6
ChacaS0 <bastide.seb.pro@gmail.com>
2018-05-11 07:41:47 +0400

    API mocking server in place

--- 01-tuto/angular_tour_of_heroes/lib/src/hero_service/hero_service.dart
+++ 01-tuto/angular_tour_of_heroes/lib/src/hero_service/hero_service.dart
@@ -1,13 +1,44 @@
-import 'package:angular/angular.dart';
 import 'dart:async';
+import 'dart:convert';
+
+import 'package:angular/angular.dart';
+import 'package:http/http.dart';
 
 import '../hero.dart';
-import 'mock_heroes.dart';
+
 
 @Injectable()
 class HeroService{
-	Future<List<Hero>> getHeroes() async => mockHeroes;
+  static const _heroesUrl = 'api/heroes'; // URL to web API
+
+  final Client _http;
+
+  HeroService(this._http);
+
+
+  //* OLD - pre-api
+	// Future<List<Hero>> getHeroes() async => mockHeroes;
+
+  //* With mocking web api
+  Future<List<Hero>> getHeroes() async {
+    try {
+      final response = await _http.get(_heroesUrl);
+      final heroes = _extractData(response)
+        .map((value) => new Hero.fromJson(value))
+        .toList();
+        return heroes;
+    } catch(e) {
+      throw _handleError(e);
+    }
+  }
+
+  dynamic _extractData(Response resp) => JSON.decode(resp.body)['data'];
 
   Future<Hero> getHero(int id) async => 
     (await getHeroes()).firstWhere((hero) => hero.id == id);
+
+  Exception _handleError(dynamic e) {
+    print(e); // for demo purposes only
+    return new Exception('Server error; cause: $e');
+  }
 }
