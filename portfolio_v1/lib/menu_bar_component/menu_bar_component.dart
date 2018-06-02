1eb68e8cbc11b19ef66dc3283f6fab91a2a00867
ChacaS0 <bastide.seb.pro@gmail.com>
2018-06-02 16:00:55 +0400

    protfolio_v1 - initial commit

---
+++ portfolio_v1/lib/menu_bar_component/menu_bar_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+@Component(
+    selector: 'menu-bar-component',
+    templateUrl: 'menu_bar_component.html')
+class MenuBarComponent {
+  var name = 'Angular';
+}
+
---
+++ portfolio_v1/web/main.dart
@@ -0,0 +1,6 @@
+import 'package:angular/angular.dart';
+import 'package:portfolio_v1/app_component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
